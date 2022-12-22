fa2e3cad7347bbe4d89fc245d447ec224e5c731f
OksanaKuzich <komoks255@gmail.com>
2022-12-22 21:45:02 +0200

    fixed error deploy's

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,12 +1,8 @@
 import { UserMenu } from 'components/AppBar/UserMenu';
 import { NavLink, Outlet } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import authSelectors from 'redux/auth/authSelectors';
 import { LogMenu } from './LogMenu';
 
 export const AppBar = () => {
-  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
-  // console.log(isLoggedIn);
 
   return (
     <>
@@ -15,7 +11,6 @@ export const AppBar = () => {
           <NavLink to="/">Home</NavLink>
           <NavLink to="/contacts">Contacts</NavLink>
         </nav>
-        {/* {isLoggedIn ? <UserMenu /> : <LogMenu />} */}
         <LogMenu />
         <UserMenu/>
       </header>
